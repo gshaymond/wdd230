0f744d7e13a6d53f6260ccffb74a2dfa6df14220
gshaymond <gshaymond@byui.edu>
2024-05-09 10:39:41 -0600

    update push

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,17 @@
 function getCurrentYear() {
-    return new Date(). getFullYear();
+    return new Date().getFullYear();
 }
+
+function getLastModifiedDate() {
+    return document.lastModified;
+}
+
+function updateFooter() {
+    var currentYear = getCurrentYear();
+    document.querySelector('footer p span').textContent = currentYear;
+
+    var lastModifiedDate = getLastModifiedDate();
+    document.getElementById('lastModified').textContent = 'Last modified: ' + lastModifiedDate;
+}
+
+window.onload = updateFooter;
